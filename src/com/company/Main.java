9438892c7fd9dc3bf90eb32c311530dbb81eee5f
Your Name <you@example.com>
2022-03-16 18:57:52 +0200

    Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 
 import static java.lang.Integer.MAX_VALUE;
@@ -8,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<Persoana> persoana = new ArrayList<>();
-        persoana.add(new Persoana("Popescu","Maria",35));
+        persoana.add(new Persoana("Popescu", "Maria", 35));
         persoana.add(new Persoana("Ionescu", "Ioan", 45));
         persoana.add(new Persoana("Georgescu", "Marian", 25));
         persoana.add(new Persoana("Popa", "Ileana", 20));
@@ -17,15 +18,22 @@ public class Main {
         System.out.println("Persoana cea mai tanara din lista este: " + varstaMica(persoana));
     }
 
-    public static void prezentareLista(ArrayList<Persoana>persoana){
-        for(var p :persoana){
+    public static void prezentareLista(ArrayList<Persoana> persoana) {
+        for (var p : persoana) {
             p.afisare();
         }
     }
-    public static int varstaMica(ArrayList<Persoana>persoana){
-        int a = MAX_VALUE;
-        for(var p : persoana){
-            if(p.varsta < a);
+
+    public static int  varstaMica(ArrayList<Persoana> persoana) {
+        int a = Integer.MAX_VALUE;
+        for (Persoana p : persoana) {
+            if (p.getClass() == Persoana.class && a > p.varsta) {
+
+                a = p.varsta;
+
+            }
+
+
         }
         return a;
     }
